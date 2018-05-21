acef5ac3b7d75f00a380fdff8c282e6ca5deaae8
nicklocicero <nicklocicero@gmail.com>
2018-05-21 13:47:20 -0600

    Removed commented out code.

--- src/edu/cnm/deepdive/Summation.java
+++ src/edu/cnm/deepdive/Summation.java
@@ -3,17 +3,6 @@ package edu.cnm.deepdive;
 public class Summation {
 
   public static void main(String[] args) {
-//    for (int i = 1; i < args.length; i = i + 2) {
-//      System.out.println(args[i]);
-//      System.out.println(args[i-1]);
-//    }
-//    for (String s: args){
-//      System.out.println(s);
-//    }
-//    int a = -2;
-//    for (;a<0;a++) {
-//      System.out.println("Works?");
-//    }
     int sum = 0;
     String output = "";
     for (int i = 0; i < args.length-1; i++) {
@@ -24,8 +13,6 @@ public class Summation {
     int val = Integer.parseInt(args[args.length-1]);
     sum += val;
     output += args[args.length-1] + " = " + Integer.toString(sum);
-    
     System.out.println(output);
   }
-
 }
