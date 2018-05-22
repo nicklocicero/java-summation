f3b4da19db2a10dc8ebc7d2a0811ce741ab0cfbf
nicklocicero <nicklocicero@gmail.com>
2018-05-22 13:50:22 -0600

    Method decomposition for operators.

--- src/edu/cnm/deepdive/Summation.java
+++ src/edu/cnm/deepdive/Summation.java
@@ -19,76 +19,90 @@ public class Summation {
     Stack<Double> stack = new Stack<>();
     for (int i = 0; i < args.length; i++) {
       String arg = args[i].trim().toLowerCase();
-      double val1;
-      double val2;
       switch (arg) {
         // FIXME - Replace repeated statements with method call.
         case "add":
         case "+":
-          val1 = stack.pop();
-          val2 = stack.pop();
-          stack.push(val1 + val2);
+          add(stack);
           break;
         case "sub":
         case "-":
-          val1 = stack.pop();
-          val2 = stack.pop();
-          stack.push(val1 - val2);
+          subtract(stack);
           break;
         case "mul":
         case "*":
-          val1 = stack.pop();
-          val2 = stack.pop();
-          stack.push(val1 * val2);
+          multiply(stack);
           break;
         case "div":
         case "/":
-          val1 = stack.pop();
-          val2 = stack.pop();
-          stack.push(val1 / val2);
-          break;
-        case "mod":
-        case "%":
-          val1 = stack.pop();
-          val2 = stack.pop();
-          stack.push(val1 % val2);
+          divide(stack);
           break;
         case "pow":
         case "exp":
         case "**":
         case "^":
-          val1 = stack.pop();
-          val2 = stack.pop();
-          stack.push(Math.pow(val1, val2));
+          power(stack);
           break;
+        case "mod":
+        case "%":
+          modulus(stack);
+          break;
+        case "abs":
         case "-a":
-          val1 = stack.pop();
-          val2 = stack.pop();
-          stack.push(Math.abs(val1 - val2));
+          abs(stack);
           break;
         default:
-          double val = Double.parseDouble(arg);
-          stack.push(val);
+          stack.push(Double.valueOf(arg));
           break;
       }
-      /*if (arg.equals("+")) {
-        double val1 = stack.pop();
-        double val2 = stack.pop();
-        stack.push(val1 + val2);
-      } else if (arg.equals("-")) {
-        double val1 = stack.pop();
-        double val2 = stack.pop();
-        stack.push(val1 - val2);
-      } else if (arg.equals("*")) {
-        double val1 = stack.pop();
-        double val2 = stack.pop();
-        stack.push(val1 * val2);
-      } else {
-        double val = Double.parseDouble(arg);
-        stack.push(val);
-      }*/
     }
     System.out.print("Result = ");
     System.out.printf("%,.2f%n", stack.pop());
+  }
+
+  /**
+   * @param stack
+   */
+  private static void add(Stack<Double> stack) {
+    double[] values = pop(stack, 2);
+    stack.push(values[0] + values[1]);
+  }
+  
+  private static void subtract(Stack<Double> stack) {
+    double[] values = pop(stack, 2);
+    stack.push(values[0] - values[1]);
+  }
+  
+  private static void multiply(Stack<Double> stack) {
+    double[] values = pop(stack, 2);
+    stack.push(values[0] * values[1]);
+  }
+  
+  private static void divide(Stack<Double> stack) {
+    double[] values = pop(stack, 2);
+    stack.push(values[0] / values[1]);
+  }
+  
+  private static void power(Stack<Double> stack) {
+    double[] values = pop(stack, 2);
+    stack.push(Math.pow(values[0], values[1]));
+  }
+  
+  private static void modulus(Stack<Double> stack) {
+    double[] values = pop(stack, 2);
+    stack.push(values[0] % values[1]);
+  }
+  
+  private static void abs(Stack<Double> stack) {
+    double[] values = pop(stack, 2);
+    stack.push(Math.abs(values[0] - values[1]));
+  }
+  
+  private static double[] pop(Stack<Double> stack, int numItems) {
+    double[] values = new double[numItems];
+    for (int i = 0; i < numItems; i++) {
+      values[i] = stack.pop();
+    }
+    return values;
   }
 }
