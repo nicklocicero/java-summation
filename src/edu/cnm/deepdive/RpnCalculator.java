80a6954626fe08ed7138d54a47dbd27a956345fc
nicklocicero <nicklocicero@gmail.com>
2018-05-23 14:52:23 -0600

    Added more operations and stack empty condition, message.

--- src/edu/cnm/deepdive/RpnCalculator.java
+++ src/edu/cnm/deepdive/RpnCalculator.java
@@ -20,11 +20,12 @@ public class RpnCalculator {
    */
   public static void main(String[] args) {
     Stack<Double> stack = new Stack<>();
-    for (int i = 0; i < args.length; i++) {
-      String arg = args[i].trim().toLowerCase();
+    for (String arg : args) {
+      arg = arg.trim().toLowerCase();
       switch (arg) {
         case "add":
         case "+":
+          System.out.println("Billboards, commercials, and adds...oh my! O_o");
           add(stack);
           break;
         case "sub":
@@ -33,6 +34,7 @@ public class RpnCalculator {
           break;
         case "mul":
         case "*":
+          System.out.println("The NY Times says: ");
           multiply(stack);
           break;
         case "div":
@@ -47,19 +49,40 @@ public class RpnCalculator {
           break;
         case "mod":
         case "%":
+          System.out.println("Total mod, bro! Hang 10 :D");
           modulus(stack);
           break;
         case "abs":
-        case "-a":
+          System.out.println("Beefy core! ;)");
           abs(stack);
+          break;
+        case "log10":
+        case "log":
+          System.out.println("Timber! Here come the logs! ;)");
+          log10(stack);
+          break;
+        case "pop":
+          System.out.println("Pop goes the weasel! ;)");
+          stack.pop();
+          break;
+        case "idiv":
+        case "\\":
+          idiv(stack);
+          break;
+        case "swap":
+          swap(stack);
           break;
         default:
           stack.push(Double.valueOf(arg));
           break;
       }
     }
-    System.out.print("Result = ");
-    System.out.printf("%,.2f%n", stack.pop());
+    if (stack.empty()) {
+      System.out.println("How can a stack be empty? Well, it is.");
+    } else {
+      System.out.print("Result = ");
+      System.out.printf("%,.2f%n", stack.pop());
+    }
   }
 
   /**
@@ -96,8 +119,28 @@ public class RpnCalculator {
   }
   
   private static void abs(Stack<Double> stack) {
+    double value = stack.pop();
+    stack.push(Math.abs(value));
+  }
+
+  private static void log10(Stack<Double> stack) {
+    double value = stack.pop();
+    stack.push(Math.log10(value));
+  }
+
+  private static void idiv(Stack<Double> stack) {
     double[] values = pop(stack, 2);
-    stack.push(Math.abs(values[0] - values[1]));
+    double result = values[0] / values[1];
+    double quotient = Math.floor(result);
+    double remainder = values[0] - values[1] * quotient;
+    stack.push(remainder);
+    stack.push(quotient);
+  }
+
+  private static void swap(Stack<Double> stack) {
+    double[] values = pop(stack, 2);
+    stack.push(values[0]);
+    stack.push(values[1]);
   }
   
   private static double[] pop(Stack<Double> stack, int numItems) {
